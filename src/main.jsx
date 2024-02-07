e93a9272ba5309ad0606e64bd4b7d76a7b42ad29
Rahmat-Khavari-Hyper-Island <145808562+Rahmat-Khavari-Hyper-Island@users.noreply.github.com>
2024-02-07 18:02:21 +0100

    remove the old context

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import BookProvider from './hooks/bookContext.jsx';
 import ThemeProvider from './hooks/themeContext.jsx';
-import CartProvider from './hooks/CartContext.jsx';
 import CartProvider2 from './hooks/CartContext2.jsx';
 
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
@@ -13,11 +12,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
       <BookProvider>
-        <CartProvider>
-          <CartProvider2>
-            <App />
-          </CartProvider2>
-        </CartProvider>
+        <CartProvider2>
+          <App />
+        </CartProvider2>
       </BookProvider>
     </ThemeProvider>
   </React.StrictMode>
